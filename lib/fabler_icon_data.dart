8defb0c67344fc2116c1b3b7d9f53a7fe915c2e8
miladimos <milad.jafari6210@gmail.com>
2022-11-24 14:26:27 +0330

    remove changelog file

---
+++ lib/fabler_icon_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class FablerIconData extends IconData {
+  const FablerIconData(var code)
+      : super(
+          code,
+          fontFamily: 'fabler-icons',
+          fontPackage: 'fabler_icons',
+        );
+}
